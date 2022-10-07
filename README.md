999a429bf46cb6f449bd521e448689b505b76b89
Jerzy DROZDZ <drozdz.jerzy@gmail.com>
2022-10-07 11:30:30 +0200

    Added missing files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# print-shop
+# Client UI Structure
+#### Example
 
 ## Project setup
 ```
@@ -18,6 +19,33 @@ npm run build
 ### Lints and fixes files
 ```
 npm run lint
+```
+
+### Deprecated packages
+* vuelidate-error-extractor v2.4.1 - is no longer supported by Vuejs 3.x
+
+### Reverse PROXY
+
+```javascript
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+
+const app = express();
+app.use(express.static('client'));
+
+// Add middleware for http proxying 
+const apiProxy = proxy.createProxyMiddleware('/api', { target: 'http://localhost:8080' });
+app.use('/api', apiProxy);
+
+// Render your site
+const renderIndex = (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/index.html'));
+}
+app.get('/*', renderIndex);
+
+app.listen(3000, () => {
+  console.log('Listening on: http://localhost:3000');
+});
 ```
 
 ### Customize configuration
---
+++ server.js
@@ -0,0 +1,54 @@
+const available_env = ['dev', 'uat', 'testing', 'prod'];
+
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const spdy = require('spdy');
+
+const server = express();
+const env = (process.argv.slice(2))[0];
+
+if (available_env.indexOf(env) < 0 || env === undefined) {
+    console.error('Unknown environment!');
+    console.log('Available environment parameter: ');
+    available_env.forEach(x => console.log(' ', x));
+    process.kill(process.pid);
+}
+
+const configFile = `appsettings.${env}.json`;
+
+if (!fs.existsSync(configFile)) {
+    console.error(`Config file '${configFile}' not exist!`);
+    process.kill(process.pid);
+}
+
+var appSettings = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+const apiProxy = proxy.createProxyMiddleware('/api', {target: appSettings.apiUrl});
+const serverOptions = {
+    key: fs.readFileSync(`${appSettings.serverOptions.basePath}/${appSettings.serverOptions.key}`),
+    cert: fs.readFileSync(`${appSettings.serverOptions.basePath}/${appSettings.serverOptions.cert}`)
+};
+
+const url = `https://localhost:${appSettings.port}`;
+
+server.use(cors(appSettings.corsOptions));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+server.use('/api', apiProxy);
+server.use('/', (req, res) => {
+    res.json({info: `Proxy server at ${url}`, version: process.version});
+});
+
+spdy.createServer(serverOptions, server).listen(appSettings.port, (error) => {
+    if (error) {
+        console.error(error);
+        return process.exit(1);
+    } else {
+        console.info('HTTP2 supported by server');
+        console.info(`Listening on ${url}`);
+    }
+});
+
